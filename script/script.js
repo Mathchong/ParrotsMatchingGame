64c56cde0f625868f68b1647301e468111bc0ba7
Matheus Chong <matheus.chong@gmail.com>
2022-02-01 15:30:23 -0300

    Made JS for select pair-cards and unflip wrong pair

--- script/script.js
+++ script/script.js
@@ -1,4 +1,4 @@
-let gifList=[
+let gifList = [
     'bobrossparrot.gif',
     'bobrossparrot.gif',
     'explodyparrot.gif',
@@ -14,10 +14,12 @@ let gifList=[
     'unicornparrot.gif',
     'unicornparrot.gif',]
 
-let moves=0;
+let moves = 0;
+let turnedCards = 0;
+
 
 const cardModel =
-`<div class='card' onclick="flipCard(this.querySelector('.look-front'),this.querySelector('.look-back'))">
+    `<div class='card' onclick="flipCard(this.querySelector('.look-front'),this.querySelector('.look-back'),this)">
 
     <div class='look-front face'>
         <img src='/media/front.png' alt=''>
@@ -28,34 +30,36 @@ const cardModel =
     </div>
 </div>`
 
-function cardNumber(){
+function cardNumber() {
     let number
-    let loop=true
-    let regex1Digit=/^\d{1}$/
-    let regex2Digit=/^\d{2}$/
+    let loop = true
+    let regex1Digit = /^\d{1}$/
+    let regex2Digit = /^\d{2}$/
 
 
-    while(loop){
+    while (loop) {
         number = prompt('Enter card number');
-        if(regex1Digit.test(number) || regex2Digit.test(number)){
-            if((number%2)==0){
-                loop = false;
-            }else{
+        if (regex1Digit.test(number) || regex2Digit.test(number)) {
+            if ((number % 2) == 0) {
+                if (number <= 14 && number >= 2) {
+                    loop = false;
+                }
+            } else {
                 alert("Os números devem ser pares!")
             }
         } else {
             alert("Entre somente com números!")
         }
     }
-    
-    let main = document.querySelector('.cards-container');
-    let htmlText=''
-    let auxCard=''
-    let shuffledGifs=copyAndSortGifList(number)
 
-    for (let i=0; i<number; i++){
-        auxCard= cardModel.replace('gif', shuffledGifs[i] );
-        htmlText+=auxCard;
+    let main = document.querySelector('.cards-container');
+    let htmlText = ''
+    let auxCard = ''
+    let shuffledGifs = copyAndSortGifList(number)
+
+    for (let i = 0; i < number; i++) {
+        auxCard = cardModel.replace('gif', shuffledGifs[i]);
+        htmlText += auxCard;
         console.log(auxCard)
     }
 
@@ -63,29 +67,62 @@ function cardNumber(){
 }
 
 
-function flipCard(front, back){
+function flipCard(front, back, cardElement) {
 
-front.classList.remove("look-front")
-front.classList.add("look-back")
+    if (cardElement.classList.contains("selected") || cardElement.classList.contains("pair-made")) {
+        return;
+    }
+    cardElement.classList.add("selected")
+    turnedCards += 1;
+    console.log(turnedCards)
 
-back.classList.remove("look-back")
-back.classList.add("look-front")
+    front.classList.remove("look-front")
+    front.classList.add("look-back")
 
-setTimeout(function(){
-    front.classList.remove("look-back")
-    front.classList.add("look-front")
-},1000)
+    back.classList.remove("look-back")
+    back.classList.add("look-front")
 
-setTimeout(function(){
-    back.classList.remove("look-front")
-    back.classList.add("look-back")
-},1000)
+    if (turnedCards == 2) {
+        let click = document.querySelector(".click-permit")
+        click.classList.toggle("click-permit")
+        click.classList.toggle("click-block")
 
-moves +=1;
+        let allSelectet = document.querySelectorAll(".selected")
+
+        let card1 = allSelectet[0]
+        let card2 = allSelectet[1]
+
+        if (card1.innerHTML == card2.innerHTML) {
+            card1.classList.add("pair-made")
+            card2.classList.add("pair-made")
+
+            turnedCards = 0
+
+            click.classList.toggle("click-permit")
+            click.classList.toggle("click-block")
+
+            card1.classList.remove("selected")
+            card2.classList.remove("selected")
+
+            return;
+        }
+
+        turnedCards = 0
+
+        unflipCard()
+        unflipCard()
+
+        setTimeout(function () {
+            click.classList.toggle("click-permit")
+            click.classList.toggle("click-block")
+        }, 1000)
+
+    }
+    moves += 1;
 }
 
-function copyAndSortGifList(itens){
-    let copy =[]
+function copyAndSortGifList(itens) {
+    let copy = []
     for (let i = 0; i < itens; i++) {
         copy[i] = gifList[i];
     }
@@ -93,3 +130,28 @@ function copyAndSortGifList(itens){
 
     return copy;
 }
+
+
+/*
+setTimeout(function(){
+    cardNumber()
+},1000)
+*/
+
+function unflipCard() {
+    let cardFront = document.querySelector(".selected > .look-front")
+    let cardBack = document.querySelector(".selected > .look-back")
+    let card = document.querySelector(".selected")
+    card.classList.remove("selected")
+    setTimeout(function () {
+        cardFront.classList.remove("look-front")
+        cardFront.classList.add("look-back")
+
+        cardBack.classList.remove("look-back")
+        cardBack.classList.add("look-front")
+    }, 1000)
+
+
+}
+
+cardNumber()
